47a7019d3575a5349c61b2967a91045c5de6fd07
Mzoughi Ilyes <mzoughi.ilyes@gmail.com>
2012-08-19 02:39:44 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gito::Application.config.secret_token = '713a5e7a2700b16b2c4963c416a9aaf7f4ac2ed5d06707a6e92cd4f7ab7528eea9d0005041f44fdd354f4aa626d15108f5fd7679e7545724b6143e4ed728ff96'
